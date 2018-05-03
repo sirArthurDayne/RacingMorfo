86d3927499619333a1632c9a9b14e07b7189f016
Krosis <xavierivan2000@hotmail.com>
2018-05-02 22:06:27 -0500

    Agregando comments y solucion a problema de Indeterminado al dibujar las lineas

--- Racing2D/Racing2D-ByKrosis/src/main.cpp
+++ Racing2D/Racing2D-ByKrosis/src/main.cpp
@@ -1,5 +1,5 @@
 #include<SFML/Graphics.hpp>
-#include <iostream>
+#include<iostream>
 
 #define LOG(x) std::cout<<x<<std::endl
 
@@ -20,13 +20,13 @@ const int ANCHURA_CALLE = 2000;
 const float CAM_PROFUNIDAD = 0.84f;
 const int DISTANCIA_SEGMENTO = 200;
 
-struct Lineas
+struct HacerLineas
 {
 	float x, y, z;																//Coordenadas del centro de la linea
 	float xScreen, yScreen, wScreen;											//Coordenadas en pantalla
 	float escala;
 
-	Lineas()																	//Constructor
+	HacerLineas()																	//Constructor
 	{
 		x = y = z = 0;			
 	}
@@ -63,7 +63,7 @@ static void DibujarQuad(sf::RenderWindow& ventana, sf::Color color, int x1, int 
 	cvx.setPoint(2, sf::Vector2f(x2 + w2, y2) );								//3 punto
 	cvx.setPoint(3, sf::Vector2f(x1 + w1, y1) );								//4 punto
 
-	LOG(cvx.getLocalBounds().width);
+	//LOG(cvx.getLocalBounds().width);
 	ventana.draw(cvx);															//Dibujar textura
 }
 
@@ -75,9 +75,18 @@ int main()
 	ventana.setFramerateLimit(60);												//limita a 60FPS
 
 	//------------------------------------------RECURSOS--------------------------------------//
-	std::vector<Lineas> lineas;
+	std::vector<HacerLineas> genLineas;
 
-	int N = lineas.size();
+																				//Cantidad de lineas visibles en el mapa(1600)
+	for (int i = 0; i < 1600; i++)
+	{
+		HacerLineas linea;														//Crea un objeto de struct HacerLineas		
+		linea.z = DISTANCIA_SEGMENTO * i;										//Le asigna una profundidad (Z) a cada linea
+		
+		genLineas.push_back(linea);												//las agrega al array genLineas
+
+	}
+		int cantidadLineas = genLineas.size();									//Guarda el tamano del array dinamico
 
 	//---------------------------------------LOOP PRINCIPAL------------------------------------//
 	while ( ventana.isOpen() )
@@ -105,9 +114,23 @@ int main()
 
 		//-----------------------------------DIBUJAR EN PANTALLA----------------------------------//
 		
-		//------------------------------------DIBUJAR TEXTURA FONDO------------------------//
-		DibujarQuad(ventana, sf::Color::Blue, 500, 500, 200, 500, 300, 100);//500,500, 200,500,300,100
+		//------------------------------------DIBUJAR TEXTURAS FONDO------------------------//
+		//DibujarQuad(ventana, sf::Color::Blue, 500, 500, 200, 500, 300, 100);//500,500, 200,500,300,100
 
+		for (int i = 0; i < 300; i++)											//Generara el dibujado del mapeado
+		{
+			HacerLineas& L = genLineas[ i % cantidadLineas ];					//Crea una ref a genLineas y pasa sus datos
+
+			L.ConvertirProyeccion(0, 1500, 0);									//Convierte coordenadas
+
+																				// le asigna un color al pasto dependiendo de sus coord.
+			sf::Color hierba = (i / 3) % 2 ? sf::Color(5, 183, 29) : sf::Color(2, 145, 21); //TRUE mas claro FALSE mas oscuro
+
+			HacerLineas p = genLineas[ (i-1) % cantidadLineas +1];
+
+			DibujarQuad(ventana, hierba, 0, p.yScreen, ANCHO, 0, L.yScreen, ANCHO);
+		}
+		LOG(cantidadLineas);
 		//----------------------------------DIBUJAR CALLE----------------------------------//
 
 		//----------------------------------MOSTRAR EN PANTALLA--------------------------------//
